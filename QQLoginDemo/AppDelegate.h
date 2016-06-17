7b49ad31cd0d6d34e202b8818431d18d4717497b
zhonggaorong <458673541@qq.com>
2016-06-17 14:28:10 +0800

    Initial Commit

---
+++ QQLoginDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQLoginDemo
+//
+//  Created by 张国荣 on 16/6/17.
+//  Copyright © 2016年 BateOrganization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
